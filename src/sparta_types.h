81bb133dba8574a7aa407f947ac087649bf1bfe9
Mads <mads@math.aau.dk>
2020-10-08 13:47:19 +0200

    small fixes in examples

--- src/sparta_types.h
+++ src/sparta_types.h
@@ -4,7 +4,6 @@
 #include <string>        // For:
 #include <algorithm>     // For: sort, std set operations, iota, min/max etc., 
 #include <unordered_map> // For: paste_col
-#include <unordered_set> // For: paste_col_with_sep_intersect
 #include <cstddef>       // For: std::size_t
 
 // [[Rcpp::plugins(cpp11)]]
@@ -17,6 +16,4 @@ using vec_str = std::vector<std::string>;
 using vec_int = std::vector<int>;
 using vec_dbl = std::vector<double>;
 using umap_str_int = std::unordered_map<std::string, vec_int>;
-using uset_str = std::unordered_set<std::string>;
-
 
